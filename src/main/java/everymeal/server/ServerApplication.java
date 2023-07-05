2747bf54bf4a3893b37ec4173aa9533ea64ebb69
kyubeom <be_man@naver.com>
2023-07-05 21:25:20 +0900

    initial commit

---
+++ src/main/java/everymeal/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package everymeal.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/everymeal/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package everymeal.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
