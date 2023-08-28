665cb80f41e85a1f3c66607b22ba25ada98b26a2
kyubeom <be_man@naver.com>
2023-08-28 21:57:38 +0900

    회원가입 로직 완료 (#10)
    - 테스트 코드 작성 필요

--- src/test/java/everymeal/server/user/controller/UserControllerTest.java
+++ src/test/java/everymeal/server/user/controller/UserControllerTest.java
@@ -2,4 +2,8 @@ package everymeal.server.user.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class UserControllerTest {}
