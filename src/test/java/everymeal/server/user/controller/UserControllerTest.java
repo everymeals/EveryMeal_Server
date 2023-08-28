f7a1885287ab2b92ea01a2459004228a5e399489
kyubeom <be_man@naver.com>
2023-08-28 21:59:45 +0900

    불필요한 import 제거 (#10)

--- src/test/java/everymeal/server/user/controller/UserControllerTest.java
+++ src/test/java/everymeal/server/user/controller/UserControllerTest.java
@@ -2,7 +2,6 @@ package everymeal.server.user.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
