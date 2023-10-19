539ac32fda163f68560b0c16cfdb7895798150f3
kyubeom <be_man@naver.com>
2023-10-19 15:08:44 +0900

    feature : 유저 가입 여부 조회 API 개발

--- src/main/java/everymeal/server/user/service/UserService.java
+++ src/main/java/everymeal/server/user/service/UserService.java
@@ -19,4 +19,7 @@ public interface UserService {
     UserEmailAuthRes emailAuth(UserEmailAuthReq request, AuthenticatedUser authenticatedUser);
 
     Boolean verifyEmailAuth(UserEmailAuthVerifyReq request, AuthenticatedUser authenticatedUser);
+
+    Boolean checkRegistration(String userDeviceId);
+
 }
--- src/test/java/everymeal/server/user/controller/UserControllerTest.java
+++ src/test/java/everymeal/server/user/controller/UserControllerTest.java
@@ -118,4 +118,21 @@ class UserControllerTest extends ControllerTestSupport {
                 .andExpect(jsonPath("$.success").value(true))
                 .andExpect(jsonPath("$.message").value("OK"));
     }
+
+    @DisplayName("회원가입 여부를 확인한다.")
+    @Test
+    void checkRegistration() throws Exception {
+        // given
+        String deviceId = "123456789";
+
+        // when then
+        mockMvc.perform(
+                        get("/api/v1/users/check-registration")
+                                .queryParam("userDeviceId",deviceId)
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.message").value("OK"));
+    }
 }
