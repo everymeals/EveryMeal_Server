8a9d5691f810b1d15ebae5613ee40b8e31c37049
dldmsql <yhy5913@gmail.com>
2023-11-11 22:12:31 +0900

    style: #56 SpotlessApply

--- src/test/java/everymeal/server/meal/service/MealServiceImplTest.java
+++ src/test/java/everymeal/server/meal/service/MealServiceImplTest.java
@@ -1,7 +1,6 @@
 package everymeal.server.meal.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
