28595f00607485ce83a5e61a4aaed550783b33b7
dldmsql <yhy5913@gmail.com>
2023-11-11 21:39:03 +0900

    fix: #56 학식 레포지토리 pk 타입 오류 수정

--- src/main/java/everymeal/server/meal/repository/MealRepository.java
+++ src/main/java/everymeal/server/meal/repository/MealRepository.java
@@ -4,4 +4,4 @@ package everymeal.server.meal.repository;
 import everymeal.server.meal.entity.Meal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MealRepository extends JpaRepository<Meal, String> {}
+public interface MealRepository extends JpaRepository<Meal, Long> {}
