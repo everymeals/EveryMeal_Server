ea07745362fe1f6efb476ec5b07cb1456aa49dbf
dldmsql <yhy5913@gmail.com>
2023-11-16 23:16:47 +0900

    chore: #56 spotless

--- src/main/java/everymeal/server/meal/repository/MealDao.java
+++ src/main/java/everymeal/server/meal/repository/MealDao.java
@@ -23,23 +23,24 @@ public class MealDao {
     private final JdbcTemplate jdbcTemplate;
 
     @Transactional
-    public void saveAll(List<Meal> mealList){
-        String sql = "INSERT INTO meal ( meal_status, meal_type, menu, offered_at, price, restaurant_idx, category) "
-            + "VALUES ( ?, ?, ?, ?, ?, ?, ?)";
+    public void saveAll(List<Meal> mealList) {
+        String sql =
+                "INSERT INTO meal ( meal_status, meal_type, menu, offered_at, price, restaurant_idx, category) "
+                        + "VALUES ( ?, ?, ?, ?, ?, ?, ?)";
 
-        jdbcTemplate.batchUpdate(sql,
-            mealList,
-            mealList.size(),
-            (PreparedStatement ps, Meal meal) -> {
-                ps.setString(1, meal.getMealStatus().toString());
-                ps.setString(2, meal.getMealType().toString());
-                ps.setString(3, meal.getMenu());
-                ps.setString(4, meal.getOfferedAt().toString());
-                ps.setDouble(5, meal.getPrice());
-                ps.setLong(6, meal.getRestaurant().getIdx());
-                ps.setString(7, meal.getCategory().toString());
-
-            });
+        jdbcTemplate.batchUpdate(
+                sql,
+                mealList,
+                mealList.size(),
+                (PreparedStatement ps, Meal meal) -> {
+                    ps.setString(1, meal.getMealStatus().toString());
+                    ps.setString(2, meal.getMealType().toString());
+                    ps.setString(3, meal.getMenu());
+                    ps.setString(4, meal.getOfferedAt().toString());
+                    ps.setDouble(5, meal.getPrice());
+                    ps.setLong(6, meal.getRestaurant().getIdx());
+                    ps.setString(7, meal.getCategory().toString());
+                });
     }
 
     public Map<String, List<DayMealListGetRes>> findDayListByOfferedAtAndUniversity(
@@ -48,7 +49,7 @@ public class MealDao {
             Optional<String> campusName) {
         // 쿼리 생성
         String query =
-            """
+                """
  SELECT NEW everymeal.server.meal.controller.dto.response.DayMealListGetRes(
         COALESCE(m.idx, 0) AS mealIdx,
         COALESCE(m.offeredAt, :offeredAt) AS offeredAt,
@@ -58,14 +59,14 @@ public class MealDao {
         COALESCE(m.price, 0) AS price,
         COALESCE(m.category, 'DEFAULT') AS category,
         r.name AS restaurantName,
-        CONCAT(u.name, ' ', u.campusName) AS universityName) 
-    FROM Restaurant r 
-    JOIN (SELECT DISTINCT mealType AS mealType FROM Meal) AS meal_types ON 1=1 
-    LEFT JOIN Meal m ON m.restaurant = r AND m.offeredAt = DATE(:offeredAt) AND m.mealType = meal_types.mealType 
-    INNER JOIN University u ON r.university = u 
-    WHERE 
-    u.name = :universityName AND u.campusName = :campusName 
-    ORDER BY r.name ASC 
+        CONCAT(u.name, ' ', u.campusName) AS universityName)
+    FROM Restaurant r
+    JOIN (SELECT DISTINCT mealType AS mealType FROM Meal) AS meal_types ON 1=1
+    LEFT JOIN Meal m ON m.restaurant = r AND m.offeredAt = DATE(:offeredAt) AND m.mealType = meal_types.mealType
+    INNER JOIN University u ON r.university = u
+    WHERE
+    u.name = :universityName AND u.campusName = :campusName
+    ORDER BY r.name ASC
     """;
         // 쿼리 옵션 추가 및 실행
         List<DayMealListGetRes> result =
