d0106a0a06565dbbe92a228e67e4d72847b1a76b
dldmsql <yhy5913@gmail.com>
2024-01-07 01:02:43 +0900

    style: spotless

--- src/main/java/everymeal/server/report/dto/ReportDto.java
+++ src/main/java/everymeal/server/report/dto/ReportDto.java
@@ -8,8 +8,6 @@ import jakarta.validation.constraints.NotNull;
 public class ReportDto {
 
     public record ReportReviewReq(
-            @Schema(
-                            description = "리뷰 신고 사유를 입력해주세요.")
-                    @NotNull(message = "리뷰 신고 사유를 입력해주세요.")
+            @Schema(description = "리뷰 신고 사유를 입력해주세요.") @NotNull(message = "리뷰 신고 사유를 입력해주세요.")
                     ReportReason reason) {}
 }
