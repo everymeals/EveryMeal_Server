2f026e34739266176a37e6e42ea09dffa04b945c
kyubeomHan <kb.hyosung@gmail.com>
2024-02-06 12:45:26 +0900

    feature : CORS cookie 설정

--- src/main/java/everymeal/server/global/config/WebMvcConfig.java
+++ src/main/java/everymeal/server/global/config/WebMvcConfig.java
@@ -6,6 +6,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
@@ -17,5 +18,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(adminResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "dev.everymeal.shop:8085"
+            ) // 허용할 출처
+            .allowedMethods("*") // 허용할 HTTP method
+            .allowedHeaders("*") // 허용할 HTTP Header
+            .allowCredentials(true) // 쿠키 인증 요청 허용
+            .maxAge(60 * 60 * 24 * 30L); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
 }
