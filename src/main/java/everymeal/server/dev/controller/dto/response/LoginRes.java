603a4db22e43bf6101a5c1c8d03cecc777fa5d2d
kyubeom <be_man@naver.com>
2024-02-06 20:44:25 +0900

    feature :
    - spotlessApply

--- src/main/java/everymeal/server/dev/controller/dto/response/LoginRes.java
+++ src/main/java/everymeal/server/dev/controller/dto/response/LoginRes.java
@@ -4,13 +4,7 @@ package everymeal.server.dev.controller.dto.response;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
 public record LoginRes(
         String accessToken,
         String nickname,
